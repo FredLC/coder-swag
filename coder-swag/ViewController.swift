d6fee1b585ec2d229184b2b4df08bbabd6595e20
fredlc <fredlefevre@outlook.com>
2018-11-16 15:27:25 -0500

    Initial commit

---
+++ coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Fred Lefevre on 2018-11-16.
+//  Copyright © 2018 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
