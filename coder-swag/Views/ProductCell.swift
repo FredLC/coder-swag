7fce7ae1936c85350b0cf27f66fafaacdd78a3bc
fredlc <fredlefevre@outlook.com>
2018-11-16 19:24:44 -0500

    Created collection view and collection cell for products page

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Fred Lefevre on 2018-11-16.
+//  Copyright © 2018 Fred Lefevre. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+}
